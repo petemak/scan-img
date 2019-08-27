fdcee3a675ec96d463e4e6e6cf2ebb091916defc
Peter J. Makumbi <peter.makumbi@yahoo.com>
2019-08-27 22:40:06 +0200

    Added GUI state

--- src/cljs/scan_img/text_input.cljs
+++ src/cljs/scan_img/text_input.cljs
@@ -1,5 +1,6 @@
 (ns scan-img.text-input
   (:require [cljs.reader :as cr]
+            [reagent.core :as r]
             [re-frame.core :as rf]
             [scan-img.subs :as subs]))
 
@@ -9,22 +10,35 @@
 
 
 (defn submit-code
-  [])
+  []
+  (println "::-> submit-code ............"))
 
 
 (defn text-field
+  "Create text input area"
   []
-  [:div
-   [:form {:id "text-field"}
-    [:div {:class "form-group"}
-     [:label {:for "code-txt-field"} "Paste docker description code here"]
-     [:textarea {:id "code-txt-field" :class "form-control" :rows 10}]]]
-   [:hr]
-   [:div {:class "form-group"}
-     [:button {:type "reset"
-               :class "btn btn-danger float-left"
-               :on-click #(reset-form)} "Reset"]
-     
-     [:button {:type "button"
-               :class "btn btn-primary float-right"
-               :on-click #(submit-code)} "Scan..."]]])
+  (let [lst (r/atom {:text ""})]
+    (fn [] 
+      [:div
+       [:form {:id "text-field" :on-submit (fn [e]
+                                             (.preventDefault e))}
+        [:div {:class "form-group"}
+         [:label {:for :code-txt-field} "Paste Docker file"]
+         [:textarea {:id :code-txt-field
+                     :name :code-txt-field
+                     :class "form-control"
+                     :rows 10
+                     :on-change (fn [e]
+                                  (swap! lst :text (-> e
+                                                       .-target
+                                                       .-value)))}
+          (:text-field @lst)]]        
+        [:hr]
+        [:div {:class "form-group"}
+         [:button {:type "reset"
+                   :class "btn btn-danger float-left"
+                   :on-click #(reset-form)} "Clear"]
+         
+         [:button {:type :submit
+                   :class "btn btn-primary float-right"
+                   :on-click #(submit-code)} "Scan..."]]]])))
