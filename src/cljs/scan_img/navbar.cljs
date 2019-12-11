39010bedfcca8ec2400cf92981b05b5e1ce190ee
Peter J. Makumbi <peter.makumbi@yahoo.com>
2019-12-11 21:45:52 +0100

    Added synchronous processing

--- src/cljs/scan_img/navbar.cljs
+++ src/cljs/scan_img/navbar.cljs
@@ -2,13 +2,20 @@
   (:require [re-frame.core :as rf]
             [scan-img.subs :as subs]))
 
+(def nav-state (atom {}))
 
 (defn nav-bar
   "Generate navigation bar"
   []
   [:div.container
-   [:nav.navbar.navbar-dark.bg-primary
-    [:span.navbar-text "Docker Image Scanner"]
-    [:a {:class "nav-link"
-         :href "#"} "Login"]
-    [:span.navbar-text "Login"]]])
+   [:nav.navbar.navbar-expand-lg.navbar-dark.bg-primary
+    [:span.navbar-text "Docker Image Scanner"]  
+    [:div.collapse.navbar-collapse {:id "navbarText"}
+     [:ul.navbar-nav.mr-auto
+      [:li.nav-item
+       [:a.nav-link {:href "#"} "Login"]]]
+
+     [:span.navbar-text
+        [:a.nav-link {:href "#"} "Toggle"]]
+     [:span.navbar-text
+      [:a.nav-link {:href "#"} "Login"]]]]])
